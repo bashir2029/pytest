0fba30c94893a50872b3c3ba9e96b17e24ad2e77
Bashir Musa <107396525+bashir2029@users.noreply.github.com>
2022-06-13 09:43:01 +0100

    Add files via upload

---
+++ list-app.py
@@ -0,0 +1,15 @@
+import os
+List1 = [8, 9, 4, 3, 8, 1, 13]
+List1.reverse()
+print("The reverse list is ", List1)
+
+List2 = [21, 434, 34, 64, 67, 78, 655, 23, 43, 333, 555, 777]
+List2.sort()
+print("The sorted list is ", List2)
+
+List3 = []
+List3 = List1.copy()
+print("List3", List3)
+
+indexvalue = List2[2:6]
+print("The Index values are ", indexvalue)
